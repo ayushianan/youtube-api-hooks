f5a96754254ffb7d8aa38292dee7b968967c283c
ayushianan <ayushi2712@gmail.com>
2020-08-16 17:39:08 +0530

    initial

---
+++ src/api/youtube.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+    //baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+});
+//https://developers.google.com/youtube/v3/docs/search/list
